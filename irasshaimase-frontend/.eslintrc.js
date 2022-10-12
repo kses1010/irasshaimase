43699cf78528473a01b08ef11a2c448e4a360b0c
baekcode <baekcode@gmail.com>
2022-10-13 00:04:03 +0900

    feat(fe): 스토리북 설치 및 세팅 (테스트용 스토리북 파일 생성)

--- irasshaimase-frontend/.eslintrc.js
+++ irasshaimase-frontend/.eslintrc.js
@@ -1,86 +1,76 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   env: {
     node: true,
     browser: true,
-    jest: true,
+    jest: true
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: [
-        'airbnb-typescript',
-        'airbnb/hooks',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:prettier/recommended',
-      ],
-      parserOptions: {
-        project: ['./tsconfig.json'],
-      },
-      excludedFiles: ['*.js', '*.jsx'],
-      rules: {
-        'no-console': 'off',
-        'no-continue': 'off',
-        'class-methods-use-this': 'off',
-        'import/extensions': 'off',
-        'import/no-named-as-default': 'off',
-        'import/prefer-default-export': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'react/destructuring-assignment': 'off',
-        'react/prop-types': 'off',
-        'react/jsx-boolean-value': 'off',
-        'react/jsx-wrap-multilines': 'off',
-        'react/no-array-index-key': 'warn',
-        'react/require-default-props': 'off',
-        'react/sort-comp': 'off',
-        'react/state-in-constructor': 'off',
-        'react/static-property-placement': 'off',
-        'react/display-name': 'off',
-        'require-await': 'off',
-        '@typescript-eslint/require-await': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-use-before-define': [
-          'warn',
-          { functions: false, classes: false, variables: false, typedefs: false },
-        ],
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
-        '@typescript-eslint/naming-convention': [
-          'error',
-          {
-            selector: 'interface',
-            format: ['PascalCase'],
-          },
-        ],
-        // It's not accurate in the monorepo style
-        'import/no-extraneous-dependencies': 'off',
-        'curly': ['error', 'all'],
-      },
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    extends: ['airbnb-typescript', 'airbnb/hooks', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:prettier/recommended'],
+    parserOptions: {
+      project: ['./tsconfig.json']
     },
-    {
-      files: ['*.d.ts'],
-      rules: {
-        'spaced-comment': ['error', 'always', { markers: ['/'] }],
-      },
-    },
-    {
-      files: ['tools/**/*.js'],
-      rules: {
-        'global-require': 'off',
-      },
-    },
-  ],
+    excludedFiles: ['*.js', '*.jsx'],
+    rules: {
+      'no-console': 'off',
+      'no-continue': 'off',
+      'class-methods-use-this': 'off',
+      'import/extensions': 'off',
+      'import/no-named-as-default': 'off',
+      'import/prefer-default-export': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/destructuring-assignment': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-boolean-value': 'off',
+      'react/jsx-wrap-multilines': 'off',
+      'react/no-array-index-key': 'warn',
+      'react/require-default-props': 'off',
+      'react/sort-comp': 'off',
+      'react/state-in-constructor': 'off',
+      'react/static-property-placement': 'off',
+      'react/display-name': 'off',
+      'require-await': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-use-before-define': ['warn', {
+        functions: false,
+        classes: false,
+        variables: false,
+        typedefs: false
+      }],
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/naming-convention': ['error', {
+        selector: 'interface',
+        format: ['PascalCase']
+      }],
+      // It's not accurate in the monorepo style
+      'import/no-extraneous-dependencies': 'off',
+      'curly': ['error', 'all']
+    }
+  }, {
+    files: ['*.d.ts'],
+    rules: {
+      'spaced-comment': ['error', 'always', {
+        markers: ['/']
+      }]
+    }
+  }, {
+    files: ['tools/**/*.js'],
+    rules: {
+      'global-require': 'off'
+    }
+  }]
 };
