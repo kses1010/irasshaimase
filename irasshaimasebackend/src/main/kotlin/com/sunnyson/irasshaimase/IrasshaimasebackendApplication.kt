443aaf3df0cc4fb8ebcb17f20a875d9cccc7fb75
Sunny <thstkd92@gmail.com>
2022-10-23 19:43:07 +0900

    chore : 패키지이름 및 어플리케이션 이름 수정

---
+++ irasshaimasebackend/src/main/kotlin/com/sunnyson/irasshaimase/IrasshaimasebackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sunnyson.irasshaimase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IrasshaimasebackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<IrasshaimasebackendApplication>(*args)
+}
---
+++ irasshaimasebackend/src/test/kotlin/com/sunnyson/irasshaimase/IrasshaimasebackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sunnyson.irasshaimase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IrasshaimasebackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
