a1b758a0347dde2a272229b391b3f74422334287
Sunny <thstkd92@gmail.com>
2023-01-21 16:46:17 +0900

    chore : Spring Boot 3 및 JDK 17 새로 시작

---
+++ irasshaimase-backend/src/main/kotlin/com/irasshaimase/irasshaimasebackend/IrasshaimaseBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.irasshaimase.irasshaimasebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IrasshaimaseBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<IrasshaimaseBackendApplication>(*args)
+}
---
+++ irasshaimase-backend/src/test/kotlin/com/irasshaimase/irasshaimasebackend/IrasshaimaseBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.irasshaimase.irasshaimasebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IrasshaimaseBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
